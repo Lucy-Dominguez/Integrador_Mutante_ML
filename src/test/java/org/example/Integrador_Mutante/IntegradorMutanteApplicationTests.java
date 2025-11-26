6fa391c288c745642782746c776378f536659dda
Lucy-Dominguez <dominguez04lucy@gmail.com>
2025-11-26 13:26:33 -0300

    Commit

---
+++ src/test/java/org/example/Integrador_Mutante/IntegradorMutanteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.Integrador_Mutante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorMutanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
